312fcd01b226b19a191a60962b10e7b769a79739
Brayden Peck <brayden.peck2@gmail.com>
2021-02-14 18:04:16 -0700

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim userInput As String
+        Dim list As String = ""
+
+        Console.WriteLine("press""Q"" at any time to quit")
+        Console.WriteLine("Enter messages to store. Enter ""read"" at any time to see the list of stored messages. Enter ""clear"" at any time to clear stored messages.")
+
+        While userInput <> "Q" And userInput <> "q"
+            userInput = Console.ReadLine()
+            list = MessageList(userInput, list)
+        End While
+    End Sub
+
+    Function MessageList(input As String, list As String)
+        If input = "read" Then
+            Console.Write($"{list}")
+        ElseIf input = "Read" Then
+            Console.Write($"{list}")
+        ElseIf input = "clear" Then
+            list = ""
+        ElseIf input = "Clear" Then
+            list = ""
+        Else
+            list = $"{list} {input}" + vbNewLine
+        End If
+
+
+        Return (list)
+    End Function
+
+End Module
